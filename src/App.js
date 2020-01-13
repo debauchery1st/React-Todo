cbe30bcc45d7c5d4247be52a20b9edeec592c9f3
debauchery1st <trevor.j.martin@gmail.com>
2020-01-13 15:30:56 -0500

    internal logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { todo: [] };
+  }
+  addTask = name => {
+    const newTask = {
+      task: name,
+      id: Date.now(),
+      completed: false
+    };
+    const clone = [...this.state.todo];
+    clone.push(newTask);
+    this.setState({ todo: clone });
+  };
+  removeTask = name => {
+    const idx = this.state.todo.find(item => item.task === name).indexOf();
+    const newTodo = [
+      ...this.state.todo.slice(0, idx),
+      ...this.state.todo.slice(idx + 1)
+    ];
+    this.setState(newTodo);
+  };
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
