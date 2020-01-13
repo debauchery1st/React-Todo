38b62c2568aee2b6a87ff671853bc86eee996b46
debauchery1st <trevor.j.martin@gmail.com>
2020-01-13 15:33:05 -0500

    list component logic

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,22 @@
+import React from "react";
+import Todo from "./Todo";
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+class TodoList extends React.Component {
+  constructor() {
+    super();
+    this.state = { todo: [] };
+  }
+  render() {
+    return (
+      <ul className="TodoList">
+        {this.state.todo.map(task => (
+          <Todo item={task} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TodoList;
